173eda4b2249bf04b50e4c54df9288e0782222b9
zk-phi <zk-phi@users.noreply.github.com>
2020-12-08 00:08:28 +0900

    Initial commit

---
+++ Readme.markdown
@@ -0,0 +1,42 @@
+# company-dwim.el
+
+![screencast](./screencast.gif)
+
+Emulate `ac-dwim` behavior (of `auto-complete.el`) with `company.el`.
+
+## Usage
+
+When you press `TAB` once, `company-dwim` completes the common part
+like `company-complete-common`.
+
+When you press `TAB` again, `company-dwim` selects and completes the
+first candidate.
+
+When you press `TAB` again, `company-dwim` undoes the first candidate,
+then selects and completes the second candidate instead.
+
+You may press `TAB` again and again to cycle through the candidates.
+
+Unlike the built-in command `company-complete-common-or-cycle`,
+`company-dwim` selects and complets a candidate, where
+`company-complete-common-or-cycle` just selects. So that you do not
+need to press `RET` explicitly after selecting a candidate.
+
+Note: This behavior is NOT enabled for flex matches
+
+## Installation
+
+Load this package, bind `TAB` to `company-dwim`, and put
+`company-dwim-frontend` to `company-frontends`.
+
+``` emacs-lisp
+(require 'company-dwim)
+(define-key company-active-map (kbd "TAB") 'company-dwim)
+(setq company-frontends
+      '(company-pseudo-tooltip-unless-just-one-frontend
+        company-dwim-frontend
+        company-echo-metadata-frontend))
+```
+
+When setting `company-frontends`, you also need to remove
+`company-preview-*-frontend`s to avoid confliction.
---
+++ company-dwim.el
@@ -0,0 +1,83 @@
+(require 'company)
+(require 'cl-lib)
+
+;; ---- state
+
+(defvar company-dwim-half-committed nil)
+
+(defun company-dwim-should-commit-p ()
+  (and company-dwim-half-committed
+       (or (eq this-command 'company-abort)
+           (not (and (symbolp this-command)
+                     (string-match-p "\\`company-" (symbol-name this-command)))))))
+
+(defun company-dwim-pre-command ()
+  (when (company-dwim-should-commit-p)
+    (company-complete-selection)))
+(advice-add 'company-pre-command :before 'company-dwim-pre-command)
+
+(defun company-dwim-restore-state (&optional _)
+  (setq company-dwim-half-committed nil))
+(add-hook 'company-after-completion-hook 'company-dwim-restore-state)
+
+;; ---- the frontend
+
+(defun company-dwim-available-p ()
+  (and company-common
+       (or (eq (company-call-backend 'ignore-case) 'keep-prefix)
+           (string-prefix-p company-prefix company-common))))
+
+(defun company-dwim-expand-prefix-p ()
+  (and company-common
+       (or (eq (company-call-backend 'ignore-case) 'keep-prefix)
+           (string-prefix-p company-prefix company-common))
+       (not (string= company-prefix company-common))))
+
+(defun company-dwim-preview-show-at-point (pos completion &optional no-keep-cursor)
+  "Like `company-preview-show-at-point' but does not preserve
+cursor position when NO-KEEP-CURSOR is true."
+  (company-preview-show-at-point pos completion)
+  (when (and no-keep-cursor
+             (not (string= completion "")))
+    (let* ((after-string (overlay-get company-preview-overlay 'after-string))
+           (display (overlay-get company-preview-overlay 'display))
+           (str (or after-string display)))
+      (remove-text-properties 0 1 '(cursor) str)
+      (overlay-put company-preview-overlay (if after-string 'after-string 'display) str))))
+
+(defun company-dwim-frontend (command)
+  (cl-case command
+    (pre-command (company-preview-hide))
+    (post-command
+     (cond ((company-dwim-expand-prefix-p)
+            (company-preview-show-at-point (point) company-common))
+           ((company-dwim-available-p)
+            (company-dwim-preview-show-at-point
+             (point) (nth company-selection company-candidates)
+             company-dwim-half-committed))))
+    (hide (company-preview-hide))))
+
+;; ---- the command
+
+(defun company-dwim (&optional arg)
+  (interactive "p")
+  (cond ((company-dwim-expand-prefix-p)
+         (company-complete-common)
+         (setq company-dwim-half-committed nil))
+        ((and (not company-dwim-half-committed)
+              (company-dwim-available-p))
+         (setq company-dwim-half-committed t))
+        (t
+         (company-select-next arg))))
+
+(defun company-dwim-select-next (&optional arg)
+  (interactive "p")
+  (unless company-dwim-half-committed
+    (setq company-dwim-half-committed t))
+  (company-select-next arg))
+
+(defun company-dwim-select-previous (&optional arg)
+  (interactive "p")
+  (company-dwim-select-next (if arg (- arg) -1)))
+
+(provide 'company-dwim)
